9e254edcb4f099cfb8d277fd57b06038e1b11218
Joshua Miles <joshua.m@hackerusa.com>
2021-07-01 20:50:58 -0500

    solution

--- public/getAll.js
+++ public/getAll.js
@@ -1,21 +1,9 @@
 // Utility for making multiple fetch requests at the same time
-function getAll(url1, url2, url3, url4) {
+function getAll(...urls) {
     let promises = []
-    if (url1) {
-        let promise = fetch(url1)
+    urls.forEach(url => {
+        let promise = fetch(url)
         promises.push(promise.then(res => res.json()))
-    }
-    if (url2) {
-        let promise = fetch(url2)
-        promises.push(promise.then(res => res.json()))
-    }
-    if (url3) {
-        let promise = fetch(url3)
-        promises.push(promise.then(res => res.json()))
-    }
-    if (url4) {
-        let promise = fetch(url4)
-        promises.push(promise.then(res => res.json()))
-    }
+    })
     return Promise.all(promises)
 }
